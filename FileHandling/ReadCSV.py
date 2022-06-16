07ceea4bda8f81000266f0f6691d4e4f63697788
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-06-16 11:55:15 +0530

    Update ReadCSV.py

--- FileHandling/ReadCSV.py
+++ FileHandling/ReadCSV.py
@@ -11,14 +11,3 @@ with open(filename, 'r') as csvfile:
     fields = next(csvreader)
     for row in csvreader:
         rows.append(row)
- 
-    # get total number of rows
-    print("Total no. of rows: %d"%(csvreader.line_num))
- 
-# printing the field names
-print('Field names are:' + ', '.join(field for field in fields))
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
-    for col in row:
-        print("%10s"%col,end=" "),
-    print('\n')
